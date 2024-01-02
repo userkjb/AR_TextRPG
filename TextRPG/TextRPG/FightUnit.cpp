da9ba4032e27fbdfe223b064e316442cd34ca58e
kjb <kjb_jh@naver.com>
2024-01-02 14:24:27 +0900

    Add FightUnit Function!!!
    # FightUnit에 들어갈 기능들 구현.
    # 확정된 공격력을 담는 변수.
    # 그 공격력을 가져올 수 있는 Get함수.
    # 대미지 로직 함수.
    # 대미지 랜더 함수 추가.

--- TextRPG/TextRPG/FightUnit.cpp
+++ TextRPG/TextRPG/FightUnit.cpp
@@ -1,5 +1,33 @@
 #include "FightUnit.h"
+#include <iostream>
 
 FightUnit::FightUnit()
 {
+}
+
+int FightUnit::GetRandomAtt()
+{
+	int Damage = rand() % ((MaxAtt - MinAtt) + 1);
+	CurDamage = MinAtt + Damage;
+	return MinAtt + Damage;
+}
+
+int FightUnit::GetRandomSpeed() const
+{
+	return rand() % (Speed + 1);
+}
+
+void FightUnit::DamageLogic(FightUnit& _Unit)
+{
+	Hp -= _Unit.GetRandomAtt();
+
+	if (Hp < 0)
+	{
+		Hp = 0;
+	}
+}
+
+void FightUnit::DamageRender()
+{
+	printf_s("%s 가 %d의 공격력으로 공격했습니다.\n", Name, CurDamage);
 }
--- TextRPG/TextRPG/FightUnit.h
+++ TextRPG/TextRPG/FightUnit.h
@@ -5,5 +5,16 @@ class FightUnit : public StatusUnit
 {
 public :
 	FightUnit();
+
+	virtual int GetRandomAtt();
+
+	int GetRandomSpeed() const;
+
+	void DamageLogic(FightUnit& _Unit);
+
+	void DamageRender();
+
+protected :
+	int CurDamage = 0;
 };
 
