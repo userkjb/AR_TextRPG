83b78c657a6688545d3b024ddcc63fe3b86a3280
kjb <kjb_jh@naver.com>
2024-01-02 14:27:58 +0900

    Player is go Town? FightZone??
    # 플레이어가 마을에 갈지 던전에 갈지 선택하는 것과.
    # 선택했을 때 실행되는 로직을 구현.

--- TextRPG/TextRPG/TextRPG.cpp
+++ TextRPG/TextRPG/TextRPG.cpp
@@ -3,32 +3,39 @@
 
 #include "Player.h"
 #include "Monster.h"
+#include "FightZone.h"
+#include "Town.h"
 
 int main()
 {
+    int Value = 0;
+    __int64 Seed = reinterpret_cast<__int64>(&Value);
+    srand(static_cast<unsigned int>(Seed));
+
     Player NewPlayer = Player();
+    NewPlayer.SetName("Player");
     Monster NewMonster = Monster();
 
-    NewMonster.SetName("Monater");
+    FightZone NewFightZone;
+    Town NewTownZone;
 
     while (true)
     {
-        NewPlayer.SetName("Player");
 
         printf_s("어디로 가시겠습니까?\n");
         printf_s("1. 마을.\n");
         printf_s("2. 사냥터.\n");
+        
         int Select = _getch();
-
         system("cls");
 
         switch (Select)
         {
         case '1' :
-            printf_s("111");
+            NewTownZone.In(NewPlayer);
             break;
         case '2' :
-            printf_s("222");
+            NewFightZone.In(NewPlayer);
             break;
         default :
             break;
