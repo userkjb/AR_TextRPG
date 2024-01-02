cc979a5d09b909f1eec2f6e096b912d2330ff9ed
CofMH <kjb_jh@naver.com>
2024-01-02 12:59:11 +0900

    Main Function in while to switch
    # 매인 함수에 while 문 추가해서 끝나지 않도록 함.
    # Text RPG의 시작 메뉴와 비슷하게 출력 되도록 printf_s 추가.
    # switch 문을 넣어서 마을과 사냥터로 갈 수 있도록 기능 추가.
    # 키보드 입력을 위한 #include <conio,h> 를 추가하여 _getch()함수를 사용하여 키보드 값을 받을 수 있도록 함.

--- TextRPG/TextRPG/TextRPG.cpp
+++ TextRPG/TextRPG/TextRPG.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <conio.h>
 
 #include "Player.h"
 #include "Monster.h"
@@ -8,9 +9,31 @@ int main()
     Player NewPlayer = Player();
     Monster NewMonster = Monster();
 
-    NewPlayer.SetName("Player");
     NewMonster.SetName("Monater");
 
-    NewPlayer.StatusRender();
-    NewMonster.StatusRender();
+    while (true)
+    {
+        NewPlayer.SetName("Player");
+
+        printf_s("어디로 가시겠습니까?\n");
+        printf_s("1. 마을.\n");
+        printf_s("2. 사냥터.\n");
+        int Select = _getch();
+
+        system("cls");
+
+        switch (Select)
+        {
+        case '1' :
+            printf_s("111");
+            break;
+        case '2' :
+            printf_s("222");
+            break;
+        default :
+            break;
+        }
+
+        system("cls");
+    }
 }
