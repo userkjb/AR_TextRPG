2280af5d898837eeda04df4106d086bd3952a844
kjb <kjb_jh@naver.com>
2024-01-02 14:49:43 +0900

    Weapon Upgrade Function
    # 무기 강화 기능 구현.

--- TextRPG/TextRPG/Town.cpp
+++ TextRPG/TextRPG/Town.cpp
@@ -1,6 +1,7 @@
 #include "Town.h"
 #include <iostream>
 #include <conio.h>
+#include "Player.h"
 
 void Town::In(Player& _Player)
 {
@@ -27,5 +28,52 @@ void Town::In(Player& _Player)
 
 void Town::WeaponUp(Player& _Player)
 {
+	while (true)
+	{
+		system("cls");
 
+		int EquipUp = _Player.Weapon.GetEquipUp();
+
+		_Player.StatusRender();
+		printf_s("---------------------------------------\n");
+		printf_s("강화수치 %d\n", EquipUp);
+		printf_s("---------------------------------------\n");
+		printf_s("1. 강화\n");
+		printf_s("2. 나가기\n");
+
+		int Select = _getch();
+
+		switch (Select)
+		{
+		case '1' :
+		{
+			int Random = rand() % 100;
+			if (Random != 0)
+			{
+				_Player.Weapon.EquipLevelUp();
+				printf_s("강화에 성공했습니다.\n");
+			}
+			else if (Random == 0)
+			{
+				int CurEquipUp = _Player.Weapon.GetEquipUp();
+				if (10 <= CurEquipUp && 20 >= CurEquipUp)
+				{
+					_Player.Weapon.EquipLevelDown(5);
+				}
+				else if (20 <= CurEquipUp)
+				{
+					_Player.Weapon.EquipLevelReset();
+				}
+
+				printf_s("강화에 실패했습니다.\n");
+			}
+			int Test = _getch();
+			break;
+		}
+		case '2':
+			break;
+		default :
+			break;
+		}
+	}
 }
